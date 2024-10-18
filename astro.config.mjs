1bd6a5eb2b7e0354af64ddf8378510a224acc8ca
sota1235 <sota1235@gmail.com>
2024-10-18 12:47:46 +0900

    modify: uninstall react plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
-import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -14,9 +10,6 @@ export default defineConfig({
     integrations: [
         mdx(),
         sitemap(),
-        react({
-            include: ['**/react/*'],
-        }),
         tailwind(),
     ],
 });
