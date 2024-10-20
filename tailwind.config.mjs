5ae196567f65bee2cb92dfc34300c93148a835b6
sota1235 <sota1235@gmail.com>
2024-10-20 16:20:39 +0900

    modify: implement blog article style[wip]

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -9,5 +11,7 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		typography,
+	],
 }
