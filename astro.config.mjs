93bd8e54331f4611da6c05431d71d69a44c9fce3
sota1235 <sota1235@gmail.com>
2024-10-23 14:35:07 +0900

    modify: run formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sota1235.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind(),
-    ],
-});
+  site: 'https://sota1235.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,16 +2,14 @@ import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				black: '#154B5F',
-				'black_hover': '#0E789F',
-			},
-		},
-	},
-	plugins: [
-		typography,
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#154B5F',
+        black_hover: '#0E789F',
+      },
+    },
+  },
+  plugins: [typography],
 }
