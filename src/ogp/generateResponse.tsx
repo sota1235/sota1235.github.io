e2e6bcb90cbf79055230cf9735ba0b653b9f223a
sota1235 <sota1235@gmail.com>
2024-10-31 12:35:20 +0900

    modify: improve ogp design

--- src/ogp/generateResponse.tsx
+++ src/ogp/generateResponse.tsx
@@ -1,35 +1,65 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from '@vercel/og'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from '../../tailwind.config.mjs'
-import { readFile } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises'
 
 const { theme } = resolveConfig(tailwindConfig)
 
-const notoSansBoldFont = () => {
-  const url = new URL('../../public/fonts/NotoSansJP-ExtraBold.woff', import.meta.url);
+const notoSansRegularFont = () => {
+  const url = new URL(
+    '../../public/fonts/NotoSansJP-Regular.woff',
+    import.meta.url
+  )
   return readFile(url)
-};
+}
+
+const notoSansBoldFont = () => {
+  const url = new URL(
+    '../../public/fonts/NotoSansJP-ExtraBold.woff',
+    import.meta.url
+  )
+  return readFile(url)
+}
 
 export async function getOgpResponse({ title }: { title: string }) {
-  const notoSansBoldFontData = await notoSansBoldFont();
+  const notoSansRegularFontData = await notoSansRegularFont()
+  const notoSansBoldFontData = await notoSansBoldFont()
 
   return new ImageResponse(
     (
-      <div style={{
-        fontSize: '3rem',
-        fontFamily: 'Noto Sans JP',
-        color: theme.colors.black,
-        background: "white",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        textAlign: "center",
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
-        <span lang="ja-JP">
+      <div
+        lang="ja-JP"
+        style={{
+          fontFamily: 'Noto Sans JP',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px 10px',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <p
+          style={{
+            fontWeight: 700,
+            color: theme.colors.black,
+            fontSize: '3rem',
+          }}
+        >
           {title}
-        </span>
+        </p>
+        <p
+          style={{
+            fontWeight: 300,
+            fontSize: '2.5rem',
+            color: theme.colors.black_hover,
+          }}
+        >
+          sota1235.com
+        </p>
       </div>
     ),
     {
@@ -38,8 +68,14 @@ export async function getOgpResponse({ title }: { title: string }) {
       fonts: [
         {
           name: 'Noto Sans JP',
+          data: notoSansRegularFontData,
+          weight: 300,
+        },
+        {
+          name: 'Noto Sans JP',
           data: notoSansBoldFontData,
-        }
+          weight: 700,
+        },
       ],
     }
   ) as Response
