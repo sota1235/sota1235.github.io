8cb0fc8b7370e19dd12e50791ce3e7b46aa565f8
sota1235 <sota1235@gmail.com>
2024-11-06 16:16:39 +0900

    modify: implement ga tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,21 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 import fetchMediaData from './src/integrations/fetch-media-data'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sota1235.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), fetchMediaData()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    fetchMediaData(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 })
